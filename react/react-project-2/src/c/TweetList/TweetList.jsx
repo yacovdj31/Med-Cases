9e6c4cbcb7c72c460893f73321f3e908e36e0325
yacovdj31 <146312232+yacovdj31@users.noreply.github.com>
2023-12-19 16:26:15 +0200

    branch 1

---
+++ react/react-project-2/src/c/TweetList/TweetList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CreateTweet = ({ tweetText, onTweetChange, onTweetSubmit }) => {
+  return (
+    <div className='create'>
+      <div className="tweet-container">
+        <div className="input-wrapper">
+
+        <textarea
+
+         value={tweetText}
+         onChange={onTweetChange}
+         placeholder=" What have you in mind..."
+         maxLength={140}/>
+          <div className="input-controls">
+            <p className="count">{tweetText.length}</p>
+            <button onClick={onTweetSubmit} disabled={tweetText.length > 140}>
+              Tweet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateTweet;
+
